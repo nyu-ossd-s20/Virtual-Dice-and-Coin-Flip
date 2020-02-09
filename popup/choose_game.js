9b6bf21cdc3d1e5fd89b9f37c1d1d9a140042438
kmaraj <kmaraj913@gmail.com>
2020-02-09 17:15:41 -0500

    games individually clickable
    now you can control which game you want to play by clicking on them separately

--- popup/choose_game.js
+++ popup/choose_game.js
@@ -6,15 +6,38 @@ const hidePage = `body > :not(.pick_game-image) {
                     display: none;
                   }`;
 
-/**
- * Listen for clicks on the buttons, and send the appropriate message to
- * the content script in the page.
- */
-function listenForClicks() {
-  document.addEventListener("click", (e) => {
 
-        // all of the dice roll execution
-        var randomNumber = Math.floor((Math.random() * 6));
+document.getElementById("coinID").addEventListener("click", flipCoin);
+
+
+function flipCoin(event) {
+  var outcome = document.querySelector('.outcome');
+          
+          var randomNumber = getRandomNumber();
+          outcome.textContent = '';
+          outcome.classList.toggle('flip');
+          outcome.classList.add('toss');
+            
+            // Waits 3sec to display flip result
+          setTimeout(function() {
+            if (randomNumber == 1) {
+              outcome.textContent = 'heads';
+            } else if (randomNumber == 2) {
+              outcome.textContent = 'tails';
+            }
+            outcome.classList.remove('toss');
+          }, 800);
+              
+      
+    function getRandomNumber() {
+      return Math.floor(Math.random() * (2 - 1 + 1)) + 1
+    }
+}
+
+document.getElementById("dieID").addEventListener("click", rollDie);
+
+function rollDie(event){
+var randomNumber = Math.floor((Math.random() * 6));
             if (randomNumber==1){
               rollDice(1);
 
@@ -40,28 +63,9 @@ function listenForClicks() {
               rollDice(6);
             }
 
-          // all of the coin flip execution
-          var outcome = document.querySelector('.outcome');
-          
-          var randomNumber = getRandomNumber();
-          outcome.textContent = '';
-          outcome.classList.toggle('flip');
-          outcome.classList.add('toss');
-            
-            // Waits 3sec to display flip result
-          setTimeout(function() {
-            if (randomNumber == 1) {
-              outcome.textContent = 'heads';
-            } else if (randomNumber == 2) {
-              outcome.textContent = 'tails';
-            }
-            outcome.classList.remove('toss');
-          }, 800);
-              
-      
-    function getRandomNumber() {
-      return Math.floor(Math.random() * (2 - 1 + 1)) + 1
-    }
+
+
+
 
     function rollDice(num) {
         const dice = [...document.querySelectorAll(".die-list")];
@@ -75,57 +79,115 @@ function listenForClicks() {
         die.classList.toggle("odd-roll");
         die.classList.toggle("even-roll");
     }
-    /**
-     * get the game URL and
-     * send a "pick_game" message to the content script in the active tab.
-     */
-    function pick_game(tabs) {
-      //browser.tabs.insertCSS({code: hidePage}).then(() => {
-        let url = gameNameToURL(e.target.textContent);
-        browser.tabs.sendMessage(tabs[0].id, {
-          command: "pick_game",
-          gameURL: url
-        });
-    }
-
-    /**
-     * Remove the page-hiding CSS from the active tab,
-     * send a "reset" message to the content script in the active tab.
-     */
-    function reset(tabs) {
-      browser.tabs.removeCSS({code: hidePage}).then(() => {
-        browser.tabs.sendMessage(tabs[0].id, {
-          command: "reset",
-        });
-      });
-    }
-
-    /**
-     * Just log the error to the console.
-     */
-    function reportError(error) {
-      console.error(`Could not pick_game: ${error}`);
-    }
-
-    /**
-     * Get the active tab,
-     * then call "pick_game()" or "reset()" as appropriate.
-     */
-    if (e.target.classList.contains("game")) {
-      browser.tabs.query({active: true, currentWindow: true})
-        .then(pick_game)
-        .catch(reportError);
-    }
-    else if (e.target.classList.contains("reset")) {
-      browser.tabs.query({active: true, currentWindow: true})
-        .then(reset)
-        .catch(reportError);
-    }
-  });
 }
 
 
 
+/**
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
+// function listenForClicks() {
+  
+//   document.addEventListener("click", (e) => {
+
+//         // all of the dice roll execution
+//         var randomNumber = Math.floor((Math.random() * 6));
+//             if (randomNumber==1){
+//               rollDice(1);
+
+//               //return browser.extension.getURL("images/dice1.png");
+//             }
+//              if (randomNumber==2){
+//               rollDice(2);
+//               //return browser.extension.getURL("images/dice2.png");
+//             }
+//              if (randomNumber==3){
+//               rollDice(3);
+//               //return browser.extension.getURL("images/dice3.png");
+//             }
+//              if (randomNumber==4){
+//               rollDice(4);
+//               //return browser.extension.getURL("images/dice4.png");
+//             }
+//              if (randomNumber==5){
+//               rollDice(5);
+//               //return browser.extension.getURL("images/dice5.png");
+//             }
+//             if (randomNumber==6){
+//               rollDice(6);
+//             }
+
+
+
+
+
+//     function rollDice(num) {
+//         const dice = [...document.querySelectorAll(".die-list")];
+//         dice.forEach(die => {
+//             toggleClasses(die);
+//             die.dataset.roll = num;
+//         });
+//     }
+
+//     function toggleClasses(die) {
+//         die.classList.toggle("odd-roll");
+//         die.classList.toggle("even-roll");
+//     }
+//     /**
+//      * get the game URL and
+//      * send a "pick_game" message to the content script in the active tab.
+//      */
+//     function pick_game(tabs) {
+//       //browser.tabs.insertCSS({code: hidePage}).then(() => {
+//         let url = gameNameToURL(e.target.textContent);
+//         browser.tabs.sendMessage(tabs[0].id, {
+//           command: "pick_game",
+//           gameURL: url
+//         });
+//     }
+
+//     /**
+//      * Remove the page-hiding CSS from the active tab,
+//      * send a "reset" message to the content script in the active tab.
+//      */
+//     function reset(tabs) {
+//       browser.tabs.removeCSS({code: hidePage}).then(() => {
+//         browser.tabs.sendMessage(tabs[0].id, {
+//           command: "reset",
+//         });
+//       });
+//     }
+
+//     /**
+//      * Just log the error to the console.
+//      */
+//     function reportError(error) {
+//       console.error(`Could not pick_game: ${error}`);
+//     }
+
+//     /**
+//      * Get the active tab,
+//      * then call "pick_game()" or "reset()" as appropriate.
+//      */
+//     if (e.target.classList.contains("game")) {
+//       browser.tabs.query({active: true, currentWindow: true})
+//         .then(pick_game)
+//         .catch(reportError);
+//     }
+//     else if (e.target.classList.contains("reset")) {
+//       browser.tabs.query({active: true, currentWindow: true})
+//         .then(reset)
+//         .catch(reportError);
+//     }
+//   });
+// }
+
+
+function reply_click(clicked_id)
+  {
+      alert(clicked_id);
+  }
 
 
 /**
